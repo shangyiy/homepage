c2d45f5274c77bd59f722233d33dc80066737ee5
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-08-10 00:39:30 +0800

    Create Side bar with antd Layout

--- src/components/RenderContent.js
+++ src/components/RenderContent.js
@@ -7,28 +7,76 @@ import { Projects } from '../pages/Projects';
 import { Volunteer } from '../pages/Volunteer';
  
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Menu, Icon } from 'antd';
 
-export function RenderContent () {
-    return (
-        <Router>
-            <div className="main-content">
-                <div className="side-bar">
-                    <Avatar />
-                    <div className='btn-group'>
-                        <button> <Link to="/">Home</Link> </button>
-                        <button> <Link to="/experience">Experience</Link></button>
-                        <button><Link to="projects">Projects</Link> </button>
-                        <button><Link to="volunteer">Volunteer</Link> </button>
-                        <button>Links</button>
-                    </div>
-                </div>
-                <div className="body">
-                    <Route exact path="/" component={Home} />
-                    <Route path="/experience" component={Experience} />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/volunteer" component={Volunteer} />
-                </div>
-            </div>
-        </Router>
-    )
+const { Content, Sider } = Layout;
+
+export class RenderContent extends React.Component {
+
+    render () {
+        return (
+            <Router>
+                <Layout>
+                    <Sider 
+                        style={{ 
+                            height: '100vh',
+                            position: "fixed",
+                            left: 0,
+                            top: 0,
+                        }}
+                    >
+                        <Avatar />
+                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                            <Menu.Item key="1">
+                                <Icon type="home" />
+                                <span>Home</span>
+                                <Link to="/" />
+                            </Menu.Item>
+
+                            <Menu.Item key="2">
+                                <Icon type="project" />
+                                <span>Projects</span>
+                                <Link to="/projects" />
+                            </Menu.Item>
+
+                            <Menu.Item key="3">
+                                <Icon type="code" />
+                                <span>Experience</span>
+                                <Link to="/experience" />
+                            </Menu.Item>
+
+                            <Menu.Item key="4">
+                                <Icon type="heart" />
+                                <span>Volunteer</span>
+                                <Link to="/volunteer" />
+                            </Menu.Item>
+
+                            <Menu.Item key="5">
+                                <Icon type="linkedin" />
+                                <span>LinkedIn</span>
+                                <a href="https://linkedin.com/in/shang-yi-yu/">Content</a>
+                            </Menu.Item>
+
+                            <Menu.Item key="6">
+                                <Icon type="profile" />
+                                <span>Resume</span>
+                                <a href="/" target='_blank'>Content</a>
+                            </Menu.Item>
+                            
+                        </Menu>
+                    </Sider>
+
+                    <Layout style={{ marginLeft: 200, top: 0}}>
+                        <Content style={{ margin: '16px 16px', padding: 24, background: '#fff', minHeight: 700 }}>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/experience" component={Experience} />
+                            <Route path="/projects" component={Projects} />
+                            <Route path="/volunteer" component={Volunteer} />
+                        </Content>
+                    </Layout>
+
+                </Layout>
+            </Router>
+        )
+    }
 }
