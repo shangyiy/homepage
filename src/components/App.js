5a6c21fef944188f440ce5fcf8c515cecc6f568b
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-08-09 23:15:38 +0800

    Work on side-bar css manually

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../css/App.css';
 
-import { Avatar } from './Avatar';
 import { RenderContent } from './RenderContent';
 
 class App extends React.Component{
   render() {
     return (
       <div className="app">
-        <Avatar />
         <RenderContent />
       </div>
     )
--- src/components/RenderContent.js
+++ src/components/RenderContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/RenderContent.css';
+import { Avatar } from './Avatar';
 import { Home } from '../pages/Home';
 import { Experience } from '../pages/Experience';
 import { Projects } from '../pages/Projects';
@@ -11,12 +12,15 @@ export function RenderContent () {
     return (
         <Router>
             <div className="main-content">
-                <div className="tool-bar">
-                    <button> <Link to="/">Home</Link> </button>
-                    <button> <Link to="/experience">Experience</Link></button>
-                    <button><Link to="projects">Projects</Link> </button>
-                    <button><Link to="volunteer">Volunteer</Link> </button>
-                    <button>Links</button>
+                <div className="side-bar">
+                    <Avatar />
+                    <div className='btn-group'>
+                        <button> <Link to="/">Home</Link> </button>
+                        <button> <Link to="/experience">Experience</Link></button>
+                        <button><Link to="projects">Projects</Link> </button>
+                        <button><Link to="volunteer">Volunteer</Link> </button>
+                        <button>Links</button>
+                    </div>
                 </div>
                 <div className="body">
                     <Route exact path="/" component={Home} />
