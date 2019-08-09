f8888e5958c6b85fac04cb0062930c1bc75ac3d2
Shang Yi <shang.yi.yu.tw@gmail.com>
2019-08-09 20:26:23 +0800

    Refactor react template

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Avatar } from './Avatar';
+import { RenderContent } from './RenderContent';
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="app">
+        <Avatar />
+        <RenderContent />
+      </div>
+    )
+  }
 }
 
 export default App;
