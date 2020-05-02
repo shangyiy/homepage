caedc426b34b99a3bc6fe2de0424faf8c69281a0
Shang-Yi <shangyi_yu@trendmicro.com>
2020-05-02 15:07:56 +0800

    Updates

---
+++ precache-manifest.50b5f33c26f22e1e1614ca424578310d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8a8f90dacdbbcfaf0b2a99af161392",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "b010977a61958f8d0cd3",
+    "url": "/homepage/static/css/2.0ba87e64.chunk.css"
+  },
+  {
+    "revision": "9c9d5cd2c28af0c16480",
+    "url": "/homepage/static/css/main.40bf9535.chunk.css"
+  },
+  {
+    "revision": "b010977a61958f8d0cd3",
+    "url": "/homepage/static/js/2.da163672.chunk.js"
+  },
+  {
+    "revision": "9c9d5cd2c28af0c16480",
+    "url": "/homepage/static/js/main.674d0b42.chunk.js"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "8f05a29190fcb67521cbf1a4c15cb3a9",
+    "url": "/homepage/static/media/profile.8f05a291.png"
+  }
+]);
