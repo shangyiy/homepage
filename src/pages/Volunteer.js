dfe586aa2b814ef21aa2f04d4995d044658f1f8a
Shang-Yi <shang.yi.yu.tw@gmail.com>
2021-09-20 21:56:44 +0800

    refine RWD of Card component

--- src/pages/Volunteer.js
+++ src/pages/Volunteer.js
@@ -1,67 +1,67 @@
 import React from 'react';
 import '../css/page.css';
-import { Card, Row, Col} from 'antd';
+import { Card, Row, Col } from 'antd';
 
 export class Volunteer extends React.Component {
-    render () {
+    render() {
         return (
             <div className="volunteering">
                 <h2>Volunteering</h2>
-                <br/>
+                <br />
                 {this.renderVolunteering()}
             </div>
         )
-    } 
-    
-    renderVolunteering () {
+    }
+
+    renderVolunteering() {
         return (
             <div className="div-grid">
-                <Row gutter={16} type="flex" className="card-row">
-                    <Col span={8} className='card-col'>
+                <Row>
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Volunteer Lecturer @ Erasmus In School</h3>
-                            <br/>Served as a volunteer to present culture and personal experience to Estonian high school students
+                            <br />Served as a volunteer to present culture and personal experience to Estonian high school students
                         </Card>
                     </Col>
 
-                    <Col span={8} className='card-col'>
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Volunteer @ PyCon Estonia</h3>
-                            <br/>Served as a volunteer to assist with the conference
+                            <br />Served as a volunteer to assist with the conference
                         </Card>
                     </Col>
 
-                    <Col span={8} className='card-col'>
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Student Volunteer @ World Vision</h3>
-                            <br/>Served as a volunteer to organize and record the event 30 hours famine in Taipei Tech
+                            <br />Served as a volunteer to organize and record the event 30 hours famine in Taipei Tech
                         </Card>
                     </Col>
                 </Row>
 
-                <Row gutter={16} type="flex">
-                    <Col span={8} className='card-col'>
+                <Row >
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Student Volunteer @ WowAfrica</h3>
-                            <br/> erved as a volunteer to assist a team of fourteen to promote African culture and record the event
+                            <br /> erved as a volunteer to assist a team of fourteen to promote African culture and record the event
                         </Card>
                     </Col>
 
-                    <Col span={8} className='card-col'>
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Elderly Care Volunteer @ 私立松瑞園老人養護所</h3>
-                            <br/>Served as an emotional support and companionship for elderly people
+                            <br />Served as an emotional support and companionship for elderly people
                         </Card>
                     </Col>
 
-                    <Col span={8} className='card-col'>
+                    <Col flex="1 0 25%" className='card-col'>
                         <Card className="card">
                             <h3>Student Volunteer @ 北科牡丹服務隊</h3>
-                            <br/>Served as educational support and companionship for indigenous children
+                            <br />Served as educational support and companionship for indigenous children
                         </Card>
                     </Col>
                 </Row>
             </div>
         )
     }
-} 
+}
